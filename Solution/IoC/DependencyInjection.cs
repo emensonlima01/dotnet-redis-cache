469bbb16cf2ad09b05adf52ac69b8a11c20bc768
Emenson Lima <emenson.germano@gmail.com>
2025-12-01 13:10:39 -0300

    Estrutura do Redis

--- Solution/IoC/DependencyInjection.cs
+++ Solution/IoC/DependencyInjection.cs
@@ -1,15 +1,47 @@
 using Application.UseCases;
 using Domain.Repositories;
+using Domain.Services;
 using Infrastructure.Repositories;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace IoC;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddRedisCache(configuration);
+        services.AddRepositories();
+        services.AddUseCases();
+
+        return services;
+    }
+
+    private static IServiceCollection AddRedisCache(this IServiceCollection services, IConfiguration configuration)
+    {
+        var redisConnection = configuration.GetConnectionString("Redis")
+            ?? throw new InvalidOperationException("Redis connection string not found");
+
+        services.AddSingleton<IConnectionMultiplexer>(sp =>
+            ConnectionMultiplexer.Connect(redisConnection));
+
+        services.AddSingleton<ICacheService, RedisCacheService>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         services.AddSingleton<IPaymentRepository, PaymentRepository>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
         services.AddScoped<ReceivePaymentUseCase>();
         services.AddScoped<CancelPaymentUseCase>();
 
