de58edce366553b951110fd038811877d05a3c53
Emenson Lima <emenson.germano@gmail.com>
2025-12-08 10:20:08 -0300

    Melhoria da organização do IoC

--- Solution/IoC/DependencyInjection.cs
+++ Solution/IoC/DependencyInjection.cs
@@ -1,11 +1,5 @@
-using Application.UseCases;
-using Domain.Repositories;
-using Domain.Services;
-using Infrastructure.Repositories;
-using Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using StackExchange.Redis;
 
 namespace IoC;
 
@@ -13,37 +7,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddRedisCache(configuration);
-        services.AddRepositories();
-        services.AddUseCases();
-
-        return services;
-    }
-
-    private static IServiceCollection AddRedisCache(this IServiceCollection services, IConfiguration configuration)
-    {
-        var redisConnection = configuration.GetConnectionString("Redis")
-            ?? throw new InvalidOperationException("Redis connection string not found");
-
-        services.AddSingleton<IConnectionMultiplexer>(sp =>
-            ConnectionMultiplexer.Connect(redisConnection));
-
-        services.AddSingleton<ICacheService, RedisCacheService>();
-
-        return services;
-    }
-
-    private static IServiceCollection AddRepositories(this IServiceCollection services)
-    {
-        services.AddSingleton<IPaymentRepository, PaymentRepository>();
-
-        return services;
-    }
-
-    private static IServiceCollection AddUseCases(this IServiceCollection services)
-    {
-        services.AddScoped<ReceivePaymentUseCase>();
-        services.AddScoped<CancelPaymentUseCase>();
+        services
+            .AddInfrastructure(configuration)
+            .AddApplication();
 
         return services;
     }
